91a20882841ef4957b0995a2a179acea09580a4c
김민주 <jasminekim0419@gmail.com>
2026-08-28 14:21:08 +0900

    initial commit

---
+++ src/main/java/com/example/shinchonton_backend/ShinchontonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinchonton_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinchontonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinchontonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shinchonton_backend/ShinchontonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shinchonton_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinchontonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
